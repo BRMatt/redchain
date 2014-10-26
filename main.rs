309e42437e29ba866207171ab90acc657b0c1da1
Matt Button <matthew@sigswitch.com>
2014-10-26 23:42:40 +0000

    Extract project and target environment from ARGV
    os::args() returns a vector of `String`s. We know we'll always have an
    0th entry, as that's how the program was executed. In the case of
    environment prefix, we don't if an argument was provided.
    
    The Vec type doesn't give us any way to get an item wrapped in an
    option; we need to convert it into a slice first:
    
    > Slices are a view into a block of memory represented as a pointer and
    > a length.
    
    Because slices return a view of memory, methods such as get() return a
    borrowed version of the contents of the Vec. As far as I know this
    borrow will last as long as `args` is in scope. Because the environment
    can potentially last beyond `args`, we clone the String, which gives us
    a copy of the String in a new area of memory. We then provide a default
    string value if the environment has not been specified.

--- main.rs
+++ main.rs
@@ -9,6 +9,14 @@ fn split_path<'a>(path: String) -> Vec<String> {
 }
 
 fn main() {
+  let args = os::args();
+
+  let project_prefix     = args[0].as_slice().split('/').last().unwrap().into_string();
+  let environment_prefix = args.as_slice().get(1).map( |e| { e.clone() } ).unwrap_or("dev".into_string());
+
+  println!("The project prefix is {}", project_prefix);
+  println!("The environment is {}", environment_prefix);
+
   match get_path().map(split_path) {
     Some(val) => for p in val.iter() { println!("{}", p); },
     None => println!("Where is the path?!")
