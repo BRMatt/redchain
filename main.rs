0606ac0ae63d37cda4fd1de990c6c06ba9429de4
Matt Button <matthew@sigswitch.com>
2014-10-27 00:32:41 +0000

    Use `Path` from stdlib for manipulating paths
    This way we can leverage the compiler to make sure we're doing thing's
    "better" (tm).
    
    This also introduces possible_alternatives for generating a list of
    possible executables. The idea is that the first one that exists will be
    called, so paths get gradually less-specific

--- main.rs
+++ main.rs
@@ -1,11 +1,15 @@
 use std::os;
 
-fn get_path() -> Option<String> {
-  return os::getenv("PATH");
-}
+fn possible_alternatives(project: String, environment: String, paths: Vec<Path>, command: &str) -> Vec<Path> {
+  let mut alternatives_with_environment: Vec<Path> = Vec::new();
+  let mut alternatives_without_environment: Vec<Path> = Vec::new();
 
-fn split_path<'a>(path: String) -> Vec<String> {
-  path.as_slice().split(':').map(|x| { x.into_string() }).collect()
+  for path in paths.iter() {
+    alternatives_with_environment.push(path.join(project + "-" + environment + "-" + command.into_string()));
+    alternatives_without_environment.push(path.join(project + "-" + command.into_string()));
+  }
+
+  alternatives_with_environment + alternatives_without_environment
 }
 
 fn main() {
@@ -13,12 +17,12 @@ fn main() {
 
   let project_prefix     = args[0].as_slice().split('/').last().unwrap().into_string();
   let environment_prefix = args.as_slice().get(1).map( |e| { e.clone() } ).unwrap_or("dev".into_string());
+  let executable_paths   = os::getenv("PATH").map_or(Vec::new(), os::split_paths);
 
   println!("The project prefix is {}", project_prefix);
   println!("The environment is {}", environment_prefix);
 
-  match get_path().map(split_path) {
-    Some(val) => for p in val.iter() { println!("{}", p); },
-    None => println!("Where is the path?!")
+  for alternative in possible_alternatives(project_prefix, environment_prefix, executable_paths, "bar").iter() {
+    println!("{}", alternative.display());
   }
 }
